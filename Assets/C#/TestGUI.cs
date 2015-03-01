7b4238e9e1fdc09c5f8b81ec9f2dbf33e5e2d09b
Jawad <jawadnmd@gmail.com>
2015-03-01 01:46:44 -0600

    VRGUI library included (with basic test scene)

---
+++ Assets/C#/TestGUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGUI : VRGUI {
+
+	public override void OnVRGUI()
+	{
+		GUILayout.BeginArea(new Rect(0f, Screen.height/2, Screen.width, Screen.height));
+		if (GUILayout.Button("Click Me!"))
+		{
+			Debug.Log("Clicked on the GUI!!!");
+		}
+		GUILayout.EndArea();
+	}
+}
